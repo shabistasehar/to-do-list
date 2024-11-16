6660aa26dd7104a42db191a366fe0e19ec621c65
Shabista Sehar <146700081+shabistasehar@users.noreply.github.com>
2024-11-16 13:51:48 +0530

    Add files via upload

--- to-do-list.cpp
+++ to-do-list.cpp
@@ -15,17 +15,17 @@ void addtask(){
     string taskname;
     
     
-        cout<<"Enter Task Description"<<":";
+        cout<<"\nEnter Task Description"<<":";
         cin.ignore();
         getline(cin,taskname);
         task.push_back({taskname,false});
-        cout<<"task added "<<endl;
+        cout<<"\ntask "<<taskname<<" added! "<<endl;
 
 }
 void markcomplete(){
     int index;
     int n=task.size();
-    cout<<"Enter the index of task to be marked as complete:";
+    cout<<"\nEnter the index of task to be marked as complete:";
     cin>>index;
     if(index>0 && index<=n){
         task[index-1].iscomplete=true;
@@ -54,9 +54,10 @@ void display(){
 
 using namespace std;
 int main(){
+    cout<<"welcome to the to-do-list app!";
     int choice;
     while(true){
-    cout<<"To do list\n Menu:\n 1.Add a new tast\n 2.Mark a task as done\n 3.Display tasks\n 4.Exit "<<endl;
+    cout<<"\n\n Menu:\n 1.Add a new tast\n 2.Mark a task as done\n 3.Display tasks\n 4.Exit\n "<<endl;
     cout<<"enter your choice:";
     cin>>choice;
     switch (choice)
